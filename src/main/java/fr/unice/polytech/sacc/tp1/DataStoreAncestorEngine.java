6821c721a889c8abfebd5ef25ef7eeda9e1066b4
Pascal Tung <tung.pascal@etu.unice.fr>
2016-10-21 16:02:07 +0200

    Test without ancestor

--- src/main/java/fr/unice/polytech/sacc/tp1/DataStoreAncestorEngine.java
+++ src/main/java/fr/unice/polytech/sacc/tp1/DataStoreAncestorEngine.java
@@ -3,6 +3,7 @@ package fr.unice.polytech.sacc.tp1;
 import com.google.cloud.datastore.*;
 import com.google.gson.Gson;
 import fr.unice.polytech.sacc.tp1.model.Article;
+import fr.unice.polytech.sacc.tp1.model.Shop;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,37 +11,56 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataStoreAncestorEngine extends HttpServlet {
 
     public static String SHOP_KIND = "shop";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        Datastore datastore = DatastoreOptions.defaultInstance().service();
-//        KeyFactory keyFactory = datastore.newKeyFactory().kind(SHOP_KIND);
-//        IncompleteKey key = keyFactory.kind(SHOP_KIND).newKey();
-//
-//        Article article = new Article("Crumble", 2.4, 1);
-//        Gson gson = new Gson();
-//
-//        FullEntity<IncompleteKey> articleEntry = FullEntity.builder(key)
-//            .set("prop", gson.toJson(article)).build();
-//        datastore.add(articleEntry);
+        Article item1 = new Article("Crumble", 2.4, 1);
+        Article item2 = new Article("Mille-feuille", 2.5, 1);
+
+        List<Article> catalog = new ArrayList<>();
+        catalog.add(item1); catalog.add(item2);
+        Shop shop = new Shop("Patisserie", catalog);
+
+        Datastore datastore = DatastoreOptions.defaultInstance().service();
+        KeyFactory keyFactory = datastore.newKeyFactory().kind(SHOP_KIND);
+        IncompleteKey key = keyFactory.kind(SHOP_KIND).newKey();
+        Gson gson = new Gson();
+
+        FullEntity<IncompleteKey> shopEntry = FullEntity.builder(key)
+            .set("shopname", shop.getName())
+            .set("nb_article", shop.getArticles().size())
+            .set("catalog_json", gson.toJson(shop.getArticles()))
+            .build();
+        datastore.add(shopEntry);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String shopname = request.getParameter("shopname");
-        String limit = request.getParameter("limit");
+        Long limit = Long.parseLong(request.getParameter("limit"));
 
-//        Datastore datastore = DatastoreOptions.defaultInstance().service();
-//        Query<Entity> query = Query.entityQueryBuilder().kind(ARTICLE_KIND).build();
-//        QueryResults<Entity> results = datastore.run(query);
-//
-//        response.setContentType("text/plain");
-//        PrintWriter out = response.getWriter();
-//        while (results.hasNext()) {
-//            Entity entity = results.next();
-//            out.format(entity.getString("prop"));
-//        }
+
+        Datastore datastore = DatastoreOptions.defaultInstance().service();
+        Query<Entity> query = Query.entityQueryBuilder().kind(SHOP_KIND).build();
+        QueryResults<Entity> results = datastore.run(query);
+
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        while (results.hasNext()) {
+            Entity entity = results.next();
+            Long catalogLimit = entity.getLong("nb_article");
+            if (shopname.equals(entity.getString("shopname")) && catalogLimit <= limit) {
+                out.format("shopname: %s, nb_article: %s, json: %s; key: %s",
+                    entity.getString("shopname"),
+                    entity.getLong("nb_article"),
+                    entity.getString("catalog_json"),
+                    entity.key()
+                );
+            }
+        }
     }
 }
